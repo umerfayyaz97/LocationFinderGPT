e1eb57162b0c250b0f49cf048e1e01e0fdb36c05
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2024-03-03 01:39:45 +0500

    Location finder

---
+++ app/locationFinderPydantic.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, HTTPException, Depends, status
+from pydantic import BaseModel
+from typing import Annotated
+
+app = FastAPI(
+    title="Location Finder API",
+    version="1.0.0",
+    description="API to find the location of a given address",
+    servers=[{
+        "url": "",
+        "description":"Development Server"
+    }]
+)
+
+#creating pydantic class
+class Location(BaseModel):
+    name: str
+    address: str
+
+#Dummy Data
+
+locations = {
+    "umer":Location(name="umer", address="Lahore"),
+    "ali":Location(name="ali", address="Islamabad"),
+}
+
+#dependency Function
+def get_Location_or_404(name: str)->Location:
+    loc = locations.get(name.lower())
+    if not loc: 
+        raise HTTPException(status_code=404, detail=f"Location not found for {name}")
+    return loc
+
+#routes
+
+@app.get("/locations/{name}")
+def get_person_location(name: str, location: Annotated[Location, Depends(get_Location_or_404)]):
+    return location
+
+
